cbc81674652639c072df1e2ed6cd865b370b6086
Bao Tran <151926857+tranbao01@users.noreply.github.com>
2026-03-30 14:23:35 +1100

    cartslice reducer functions implemented

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,29 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost} = action.payload
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity += 1; // Increment quantity if item already exists
+        } else {
+            state.items.push({ name, image, cost , quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+        const {name} = action.payload
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem && existingItem.quantity > 1) {
+            existingItem.quantity -= 1;
+        } else if (existingItem && existingItem.quantity === 1) {
+            state.items = state.items.filter(item => item.name !== name)
+        }
     },
     updateQuantity: (state, action) => {
-
-    
+        const {name, quantity} = action.payload
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity = quantity;
+        }
     },
   },
 });
